9d6466793fd2a26c963700cf6275b5bbfe6a83d8
ayush <139282528+ayush-jpatel@users.noreply.github.com>
2025-06-28 01:53:52 +0530

    added getQuizQuestions

--- quizzapp/quizzapp/src/main/java/com/building/quizzapp/service/QuizService.java
+++ quizzapp/quizzapp/src/main/java/com/building/quizzapp/service/QuizService.java
@@ -1,6 +1,8 @@
 package com.building.quizzapp.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 import com.building.quizzapp.dao.QuestionDao;
 import com.building.quizzapp.dao.QuizDao;
 import com.building.quizzapp.model.Question;
+import com.building.quizzapp.model.QuestionWrapper;
 import com.building.quizzapp.model.Quiz;
 
 @Service
@@ -32,4 +35,16 @@ public class QuizService {
         return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionsToUser = new ArrayList<>();
+
+        for( Question q : questionsFromDB){
+            QuestionWrapper questionWrapper = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4());
+            questionsToUser.add(questionWrapper);
+        }
+        return new ResponseEntity<>(questionsToUser, HttpStatus.OK);
+    }
 }
